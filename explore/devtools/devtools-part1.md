ac51e40d22924250c901c639842119910e061bc7
Daniel Katulevskiy <katulevskiy@gmail.com>
2024-04-30 21:11:36 -0700

    progress

---
+++ explore/devtools/devtools-part1.md
@@ -0,0 +1,9 @@
+1. citylots.json
+2. expose.js
+3. 11.69 MB, transferred 778.48 kB
+4. Waiting: 31 ms, Receiving: 114 ms. Total: 145 ms
+5. User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:124.0) Gecko/20100101 Firefox/124.0
+6. server: GitHub.com
+7. Wed, 01 May 2024 03:45:02 GMT
+8. content-type: application/json; charset=utf-8
+9. fetchData
---
+++ explore/devtools/explore.js
@@ -0,0 +1,19 @@
+// part1.js
+
+function printSum() {
+  debugger;
+  let num1 = document.getElementById("num1").value;
+  let num2 = document.getElementById("num2").value;
+  document.getElementById("sum").innerHTML = "Sum: " + calculateSum(num1, num2);
+}
+
+function calculateSum(num1, num2) {
+  let result = +num1 + +num2;
+  return result;
+}
+
+function init() {
+  document.getElementById("printSum").addEventListener("click", printSum);
+}
+
+window.addEventListener("DOMContentLoaded", init);
